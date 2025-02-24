ff93c9b6ee28230eb4bc98147c403dc8b61731a9
abdelmouiz <a_youb@estin.dz>
2025-02-24 09:16:32 -1100

    the first section is done

--- src/components/AdminSectionOne.tsx
+++ src/components/AdminSectionOne.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Cart from "./Cart";
 
-function AdminSectionOne() {
+const AdminSectionOne: React.FC = () => {
   return (
-    <div className="flex justify-center items-center flex-wrap flex-col md:flex-row"></div>
+    <div className="flex justify-center items-center flex-wrap flex-col md:flex-row gap-5">
+      <Cart text="text-white" back="" />
+      <Cart text="text-black" back="bg-white" />
+      <Cart />
+    </div>
   );
-}
+};
 
 export default AdminSectionOne;
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,12 +1,40 @@
 import React from "react";
-
-function Cart() {
+import { FcSimCardChip } from "react-icons/fc";
+import "../App.css";
+interface CartProps {
+  text?: string;
+  back?: string;
+}
+const Cart: React.FC<CartProps> = ({ text, back }) => {
   return (
-    <div className="flex justify-center items-center flex-row">
-      <div></div>
-      <div></div>
+    <div
+      className={`flex justify-center items-center flex-col  gap-5  p-3 ${text} rounded-2xl lineaire w-[300px] shadow-2xl ${back}`}
+    >
+      <div className="flex justify-between items-center flex-row gap-4 w-full ">
+        <div className="flex justify-center items-center flex-col ">
+          <p className="text-sm"> Total Revenue</p>
+          <p>$5,756,596</p>
+        </div>
+        <div className="flex justify-center items-center ">
+          <FcSimCardChip className="text-3xl text-white" />{" "}
+        </div>
+      </div>
+      <div className="flex justify-between items-center flex-row w-full ">
+        <div className="flex justify-center  items-center flex-col   ">
+          <p className="text-sm">CARD HOLDER</p>
+          <p className="text-sm">VALID THRU</p>
+        </div>
+        <div className="flex justify-center  items-center flex-col   ">
+          <p>Eddy Cusuma</p>
+          <p>12/22</p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-row gap-5  ">
+        <p className="text-xl">3778 **** **** 1234</p>
+        <FcSimCardChip className="text-3xl" />
+      </div>
     </div>
   );
-}
+};
 
 export default Cart;
