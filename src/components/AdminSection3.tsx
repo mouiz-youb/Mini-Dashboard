442b5f2ee3974216a875b26315a6dce2c59468e4
abdelmouiz <a_youb@estin.dz>
2025-02-27 07:03:44 -1100

    modefication inside the design of the admin page

--- src/components/AdminSection3.tsx
+++ src/components/AdminSection3.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import AiEngent from "./AiEngent";
 import Chart3 from "./Chat3";
-
+import Cercle from "./Cercle";
+import  Chart4  from "./Chart4";
 function AdminSection3() {
   return (
-    <div className="flex justify-center items-center flex-col gap-5 md:grid md:grid-cols-5 md:grid-rows-1 w-full p-3 " >
-      <div className="flex justify-center items-center flex-col md:col-start-1 md:col-end-3 md:row-start-1 md:row-end-1">
-        <AiEngent />
+    <div className="flex justify-center items-center flex-col gap-5 md:grid md:grid-cols-6 md:grid-rows-1 w-full p-3  " >
+      <div className="flex justify-center items-center flex-row md:col-start-1 md:col-end-4 md:gap-5 md:row-start-1 md:row-end-1">
+        <Chart4/>
       </div>
-      <div className="flex justify-center items-center flex-col md:col-start-3 md:col-end-6 md:row-start-1 md:row-end-1  ">
+      <div className="flex justify-center items-center flex-col md:col-start-4 md:col-end-7 md:row-start-1 md:row-end-1  ">
         <Chart3/>
       </div>
     </div>
@@ -16,3 +17,5 @@ function AdminSection3() {
 }
 
 export default AdminSection3;
+{/* <AiEngent /> */}
+{/* <Cercle/> */}
--- src/components/AdminSectionTwo.tsx
+++ src/components/AdminSectionTwo.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Chart2 from "./Chart2";
 import Cercle from "./Cercle";
+import AiEngent from "./AiEngent";
 
 function AdminSectionTwo() {
   return (
     <div className="w-full flex justify-center items-center flex-col gap-5 md:grid md:grid-cols-4 md:grid-rows-1 ">
       <Chart2 />
       <div className="flex justify-center items-center md:col-start-3 md:col-end-5 md:row-start-1 md:row-end-1">
-        <Cercle />
+        {/* <Cercle /> */}
+        <AiEngent/>
       </div>
     </div>
   );
